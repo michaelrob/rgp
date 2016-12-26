8994f36419978f35e2537bbbc09a6027d8572a25
Michael Robinson <seraph890@gmail.com>
2016-12-26 21:57:02 +0900

    added basic room class, we will use this to get more information to populate our room

---
+++ lib/room.rb
@@ -0,0 +1,18 @@
+class Room
+
+  def initialize
+    @content = get_content
+    @size = get_size
+    @adjective = get_adjective
+  end
+
+private
+  def get_content
+  end
+
+  def get_size
+  end
+
+  def get_adjective
+  end
+end
