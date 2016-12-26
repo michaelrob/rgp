8f8168f37a506856690e553d5a370a8270996e03
Michael Robinson <seraph890@gmail.com>
2016-12-26 22:08:46 +0900

    we dont need chomp, get out of here

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,7 @@ private
   def start_game
     while @player.alive?
       puts "you have taken #{@steps_taken} steps"
-      steps_to_take = gets.chomp.to_i
+      steps_to_take = gets.to_i
       @steps_taken += 1
     end
   end
