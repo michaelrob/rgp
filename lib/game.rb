1465032dacf064ec5cf58ec83dda1dd4cceebb48
Michael Robinson <seraph890@gmail.com>
2016-12-26 22:14:42 +0900

    added status method for showing us what room we are in, this will need to be fleshed out a bit more

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,12 @@ class Game
   ACTIONS = [
     :north, :east, :south, :west, :status
   ]
-  
+
   def initialize
     @steps = 0
+    @world = World.new
     @player = Player.new
+
     start_game
   end
 
@@ -13,10 +15,21 @@ private
 
   def start_game
     while @player.alive? && steps <= 100
+      @current_room = @world.get_room(@player)
+
+      # ToDo: we'll need to flesh this out a bit more
+      status
+
+
+
       puts "you have taken #{@steps} steps"
 
       steps_to_take = gets.to_i
       @steps += 1
     end
+  end
+
+  def status
+    puts @current_room
   end
 end
