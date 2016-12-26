41313f9527a3cbaa1a8230a0d9f92cd63c6ca43c
Michael Robinson <seraph890@gmail.com>
2016-12-26 21:54:46 +0900

    added more to the move methods, fixed up variables, we will want to call this later so maybe we can rename things as we use it

--- lib/world.rb
+++ lib/world.rb
@@ -6,19 +6,23 @@ class World
     @rooms = Array.new(HEIGHT, Array.new(WIDTH))
   end
 
-  def move_north()
+  def move_north(entity)
+    entity.y_pos -= 1 if entity.y_pos > 0
   end
 
-  def move_south()
+  def move_south(entity)
+    entity.y_pos += 1 if entity.y_pos < WORLD_HEIGHT - 1
   end
 
-  def move_east()
+  def move_east(entity)
+    entity.x_pos += 1 if entity.x_pos < WORLD_HEIGHT - 1
   end
 
-  def move_west()
+  def move_west(entity)
+    entity.x_pos -= 1 if entity.x_pos > 0
   end
 
-  def get_room()
+  def get_room(entity)
     @rooms[entity.x_pos][entity.y_pos] ||= Room.new
   end
 end
