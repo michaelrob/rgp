1e8fca931f84c6f0c00ab43c9a1c273b3ac70a69
Michael Robinson <seraph890@gmail.com>
2016-12-27 10:09:52 +0900

    added interact method to room, we will need to flesh this out later

--- lib/room.rb
+++ lib/room.rb
@@ -1,10 +1,14 @@
 class Room
   attr_accessor :size, :content, :adjective
-  
+
   def initialize
     @content = get_content
     @size = get_size
     @adjective = get_adjective
+  end
+
+  # ToDo: this needs to be fleshed out when we get some content added
+  def interact
   end
 
 private
