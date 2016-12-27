ee4dd68b074c7e8cac763d61faae7b3d3c666fb4
Michael Robinson <seraph890@gmail.com>
2016-12-27 16:28:04 +0900

    fixed up variable names again

--- lib/world.rb
+++ lib/world.rb
@@ -7,19 +7,19 @@ class World
   end
 
   def move_north(player)
-    entity.y_pos -= 1 if entity.y_pos > 0
+    player.y_pos -= 1 if player.y_pos > 0
   end
 
   def move_south(player)
-    entity.y_pos += 1 if entity.y_pos < WORLD_HEIGHT - 1
+    player.y_pos += 1 if player.y_pos < WORLD_HEIGHT - 1
   end
 
   def move_east(player)
-    entity.x_pos += 1 if entity.x_pos < WORLD_HEIGHT - 1
+    player.x_pos += 1 if player.x_pos < WORLD_HEIGHT - 1
   end
 
   def move_west(player)
-    entity.x_pos -= 1 if entity.x_pos > 0
+    player.x_pos -= 1 if player.x_pos > 0
   end
 
   def get_room(player)
